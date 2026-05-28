ea285201b4eb7a05203fdfee797e1b18a3d996df
Turkanyasar <turkanaliyevayasar@gmail.com>
2026-05-28 11:40:24 +0000

    app

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources = {r"/api/news": {
+    "origins": ["https://turkanyasar.github.io"],
+    "methods": ["GET"]
+}})
 
 
 def get_db_connection():
